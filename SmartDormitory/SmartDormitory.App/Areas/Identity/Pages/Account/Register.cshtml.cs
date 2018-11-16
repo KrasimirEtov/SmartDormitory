04acf008c1633c5f515bc2b919ee5a5011b25d92
fargo32 <imkitukov@gmail.com>
2018-11-16 17:51:56 +0200

    login with username instead of email

--- SmartDormitory/SmartDormitory.App/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SmartDormitory/SmartDormitory.App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -38,6 +38,11 @@ namespace SmartDormitory.App.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -65,7 +70,7 @@ namespace SmartDormitory.App.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User { UserName = Input.Username, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {

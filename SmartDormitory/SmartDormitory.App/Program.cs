707919989712b172e7f74f55d95a06421bd481f5
fargo32 <imkitukov@gmail.com>
2018-12-11 00:42:19 +0200

    add detailes errors for azure, minor fix mysensors filters

--- SmartDormitory/SmartDormitory.App/Program.cs
+++ SmartDormitory/SmartDormitory.App/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace SmartDormitory.App
 {
@@ -19,6 +12,8 @@ namespace SmartDormitory.App
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                   .CaptureStartupErrors(true)
+                   .UseSetting("detailedErrors", "true")
+                   .UseStartup<Startup>();
     }
 }

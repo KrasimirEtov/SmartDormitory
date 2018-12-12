51d075b44c0595056d3eb67eb4c47344774ed5a0
fargo32 <imkitukov@gmail.com>
2018-12-12 14:51:16 +0200

    home index styled

--- SmartDormitory/SmartDormitory.App/wwwroot/js/site.js
+++ SmartDormitory/SmartDormitory.App/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(function () {
+    $('#scrollToMap').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 800, 'linear');
+    });
+});

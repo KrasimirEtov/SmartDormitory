32a32db053ebdfd971ef8afc752d1acee63a3a7b
fargo32 <imkitukov@gmail.com>
2018-12-16 16:52:38 +0200

    add ApiDataHelpers tests

---
+++ SmartDormitory/SmartDormitory.Tests/SmartDormitory.ServicesTests/Utils/Helpers/ApiDataHelpers.Tests/GetLastValue_Should.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartDormitory.Services.Utils.Helpers;
+using System;
+
+namespace SmartDormitory.Tests.SmartDormitory.ServicesTests.Utils.Helpers.ApiDataHelpers.Tests
+{
+    [TestClass]
+    public class GetLastValue_Should
+    {
+        [TestMethod]
+        [DataRow("16.5")]
+        public void Return_CorrectFloatValue(string stringText)
+        {
+            var result = ApiDataHelper.GetLastValue(stringText);
+
+            Assert.AreEqual(16.5, result);
+        }
+
+        [TestMethod]
+        [DataRow("true")]
+        public void Return_CorrectValue_WhenPassedTrueAsString(string stringText)
+        {
+            var result = ApiDataHelper.GetLastValue(stringText);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        [DataRow("false")]
+        public void Return_CorrectValue_WhenPassedFalseAsString(string stringText)
+        {
+            var result = ApiDataHelper.GetLastValue(stringText);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        [DataRow("invalid string")]
+        public void ThrowsInvalidOperationException_WhenPassedInvalidString(string stringText)
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => ApiDataHelper.GetLastValue(stringText), "Invalid last value response");
+        }
+    }
+}
---
+++ SmartDormitory/SmartDormitory.Tests/SmartDormitory.ServicesTests/Utils/Helpers/ApiDataHelpers.Tests/GetMinAndMaxRange_Should.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartDormitory.Services.Utils.Helpers;
+
+namespace SmartDormitory.Tests.SmartDormitory.ServicesTests.Utils.Helpers.ApiDataHelpers.Tests
+{
+    [TestClass]
+    public class GetMinAndMaxRange_Should
+    {
+        [TestMethod]
+        [DataRow("This sensor will return values between 500 and 3500")]
+        public void Return_CorrectMinMaxValues_WhenPassedDigitsAsString(string description)
+        {
+            var result = ApiDataHelper.GetMinAndMaxRange(description);
+
+            Assert.AreEqual(500, result.MinRange);
+            Assert.AreEqual(3500, result.MaxRange);
+        }
+
+        [TestMethod]
+        [DataRow("This sensor will return true or false value")]
+        public void Return_CorrectMinMaxValues_WhenPassedBooleansAsString(string description)
+        {
+            var result = ApiDataHelper.GetMinAndMaxRange(description);
+
+            Assert.AreEqual(0, result.MinRange);
+            Assert.AreEqual(1, result.MaxRange);
+        }
+    }
+}
